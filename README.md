b7ed965b2bf75ddbe415c6c7bf3ed934f0ad3a9d
jia-baos <18383827268@163.com>
2026-03-29 19:40:15 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# 使用文档
+
+``` BASH
+source /opt/ros/jazzy/setup.bash
+
+# 安装 colcon 编译器
+sudo apt install python3-colcon-common-extensions
+
+# colcon 查看版本
+colcon version-check
+
+# 创建工作目录
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+
+# 创建工作空间
+ros2 pkg create --build-type ament_cmake --license Apache-2.0 cpp_pubsub
+
+# 自动安装相关依赖（似乎不需要执行）
+rosdep install -i --from-path src --rosdistro jazzy -y
+
+# 编译代码，ros2_ws 目录下
+colcon build --packages-select cpp_pubsub
+
+# 把路径临时加到当前终端环境
+. install/setup.bash
+
+# 启动发送节点
+ros2 run cpp_pubsub talker
+
+# 启动接收节点
+ros2 run cpp_pubsub listener
+```
---
+++ src/py_srvcli/py_srvcli/service_member_function.py
@@ -0,0 +1,35 @@
+from example_interfaces.srv import AddTwoInts
+
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
+
+
+class MinimalService(Node):
+
+    def __init__(self):
+        super().__init__('minimal_service')
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+
+    def add_two_ints_callback(self, request, response):
+        response.sum = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_service = MinimalService()
+
+    try:
+        rclpy.spin(minimal_service)
+    except KeyboardInterrupt:
+        pass
+
+    minimal_service.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
